d2772bd433ec451b2479e631d701d671148e88a9
Leonardo Bastos <leonardobastos04@gmail.com>
2024-08-10 16:23:17 -0300

    feat: adding mapping tickets and create Authenticate services

---
+++ Ryze.System.Infra/Identity/AuthenticateService.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Ryze.System.Domain.Entity.Identity;
+using Ryze.System.Domain.Interfaces.Accounts;
+using System.Web;
+
+namespace Ryze.System.Infra.Identity
+{
+    public class AuthenticateService : IAuthenticate
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+
+        public AuthenticateService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
+                                    IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+        public async Task<AuthenticationResult> Authenticate(string email, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return new AuthenticationResult { Success = false, ErrorMessage = "Usuário não encontrado." };
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user, password, false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return new AuthenticationResult { Success = true };
+            }
+
+            if (result.IsLockedOut)
+            {
+                return new AuthenticationResult { Success = false, ErrorMessage = "Conta bloqueada." };
+            }
+
+            return new AuthenticationResult { Success = false, ErrorMessage = "Credenciais inválidas." };
+        }
+
+        public async Task<RegisterResult> RegisterUserAsync(string email, string password, bool isClient)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = email,
+                Email = email,
+                IsClient = isClient
+            };
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+
+                return new RegisterResult { Success = true };
+            }
+            else
+            {
+                return new RegisterResult { Success = false, Errors = result.Errors.Select(e => e.Code) };
+            }
+        }
+
+        public async Task<string> GeneratePasswordResetTokenAsync(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                throw new Exception("Usuário não encontrado.");
+            }
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var resetUrl = $"{_configuration["FrontendUrl"]}/reset-password?token={HttpUtility.UrlEncode(token)}&email={HttpUtility.UrlEncode(email)}";
+
+            var message = $"Clique <a href='{resetUrl}'>aqui</a> para redefinir sua senha.";
+                      
+            return token;
+        }
+
+        public async Task<IdentityResult> ResetPasswordAsync(string email, string token, string newPassword)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                throw new Exception("Usuário não encontrado.");
+            }
+
+            return await _userManager.ResetPasswordAsync(user, token, newPassword);
+        }
+
+        public async Task Logout()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
+        public async Task<IdentityResult> AddRoleToUserAsync(string email, string role)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return IdentityResult.Failed(new IdentityError { Description = "User not found." });
+            }
+
+            return await _userManager.AddToRoleAsync(user, role);
+        }
+
+    }
+}
---
+++ Ryze.System.Infra/Mappings/Tickets/TicketMapping.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ryze.System.Domain.Entity.Tickets;
+
+namespace Ryze.System.Infra.Mappings.Tickets
+{
+    public class TicketMapping : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Description).IsRequired();
+
+            builder.Property(p => p.ClientImage)
+                .HasMaxLength(250);
+
+            builder.Property(p => p.OpeningDate).IsRequired();
+
+            builder.Property(p => p.Resolution);
+
+            builder.Property(p => p.UserImage)
+               .HasMaxLength(250);
+
+            builder.Property(p => p.Status)/*.HasConversion<string>()*/;
+
+            builder.Property(p => p.Nivel);
+
+            builder.Property(p => p.Priority);
+
+
+            builder.Property(p => p.OpeningDate);
+
+            builder.Property(p => p.IsActive);
+
+            builder.HasOne(t => t.Client)
+                   .WithMany()
+                   .HasForeignKey(t => t.ClientId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(t => t.User)
+                   .WithMany()
+                   .HasForeignKey(t => t.UserId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
