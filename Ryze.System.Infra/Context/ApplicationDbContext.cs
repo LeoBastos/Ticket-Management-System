4f956e09886f556e8350ea85865e1ce306c10de7
Leonardo Bastos <leonardobastos04@gmail.com>
2024-08-10 16:20:32 -0300

    feat: adding references and configure db context

---
+++ Ryze.System.Infra/Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Ryze.System.Domain.Entity.Identity;
+using Ryze.System.Domain.Entity.Tickets;
+
+namespace Ryze.System.Infra.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        { }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
