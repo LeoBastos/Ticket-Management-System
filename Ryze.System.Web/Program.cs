0a93dbdf2be66ec556c5975bb7c2c5df1cce9818
Leonardo Bastos <leonardobastos04@gmail.com>
2024-08-10 17:16:45 -0300

    feat: add migration and settings injections

--- Ryze.System.Web/Program.cs
+++ Ryze.System.Web/Program.cs
@@ -1,3 +1,18 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Ryze.System.Application.Mapping;
+using Ryze.System.Application.Services.Tickets;
+using Ryze.System.Application.Services.Users;
+using Ryze.System.Domain.Entity.Identity;
+using Ryze.System.Domain.Interfaces.Accounts;
+using Ryze.System.Domain.Interfaces.Tickets;
+using Ryze.System.Domain.Seed;
+using Ryze.System.Infra.Context;
+using Ryze.System.Infra.Identity;
+using Ryze.System.Infra.Repositories.Tickets;
+using Ryze.System.Infra.Repositories.Users;
+
 namespace Ryze.System.Web
 {
     public class Program
@@ -5,6 +20,69 @@ namespace Ryze.System.Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(connection));
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                            .AddEntityFrameworkStores<ApplicationDbContext>()
+                            .AddDefaultTokenProviders();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            });
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.Cookie.Name = "AspNetCore.Cookies";
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                    options.SlidingExpiration = true;
+                });
+
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireUserAdminGerenteRole",
+                     policy => policy.RequireRole("Cliente", "User", "Admin", "Gerente"));
+            });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("IsAdminClaimAccess",
+                    policy => policy.RequireClaim("IsAdmin", "true"));
+
+                options.AddPolicy("IsManagerClaimAccess",
+                    policy => policy.RequireClaim("IsManager", "true"));
+
+                options.AddPolicy("IsEmployeeClaimAccess",
+                    policy => policy.RequireClaim("IsEmployee", "true"));
+
+                options.AddPolicy("IsClientClaimAccess",
+                    policy => policy.RequireClaim("IsClient", "true"));
+            });
+
+            //Injection
+            builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();            
+
+            builder.Services.AddScoped<ITicketService, TicketService>();
+            builder.Services.AddScoped<ITicketRepository, TicketRepository>();
+
+            builder.Services.AddScoped<IAuthenticate, AuthenticateService>();
+
+
+
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
+            //Mapper
+            builder.Services.AddAutoMapper(typeof(ApplicationServiceMappings));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -24,13 +102,37 @@ namespace Ryze.System.Web
 
             app.UseRouting();
 
+
+
+            CriarPerfisUsuariosAsync(app);
+
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "MinhaArea",
+                pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
+
+            async Task CriarPerfisUsuariosAsync(WebApplication app)
+            {
+                var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+                using (var scope = scopedFactory?.CreateScope())
+                {
+                    var service = scope?.ServiceProvider.GetService<ISeedUserRoleInitial>();
+                    await service.SeedRolesAsync();
+                    await service.SeedUsersAsync();
+
+                }
+            }
+
+
         }
     }
 }
